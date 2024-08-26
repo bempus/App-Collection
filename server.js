4eea5543df90ef26a7d00c93d7bad377e669b993
Jonathan Svahn <bempus@q-er.live>
2024-08-26 19:32:41 +0200

    Added routing for minesweeper.bempus.net

--- server.js
+++ server.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.use("/", (req, res, next) => {
+  const host = req.get("host");
+
+  if (host.match(/minesweeper\..*/i)) {
+    return express.static(__dirname + "/static/minesweeper")(req, res, next);
+  }
+  next();
+});
+
 app.use(express.static(__dirname + "/static"));
 
 app.listen(process.env.PORT || 3000);
